998355742236965985e7c501fc8f503c8eec59c2
diced <pranaco2@gmail.com>
2021-02-02 22:20:00 -0800

    osu

---
+++ scripts/osu.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+unzip "$1" -d "$HOME/games/osu/drive_c/osu/Songs/${1%.osz}"
--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,4 +1,4 @@
-URL=$(curl -H "Content-Type: multipart/form-data" -H "authorization: $ZIP_AUTH" -F file=@$1 "https://diced.wtf/api/upload"); echo -n $URL | xsel -ib
+URL=$(curl -H "Content-Type: multipart/form-data" -H "authorization: $ZIP_AUTH" -F file=@"$1" "https://diced.wtf/api/upload"); echo -n $URL | xsel -ib
 # curl -H "Content-Type: multipart/form-data" -H "Authorization: $VCH_AUTH" -F file=@$1 https://cdn.voidchan.gg/api/providers/sharex | jq -r '.files[].url' | xsel -ib
 
 ACTION=$(dunstify --action="close,open" "Uploaded Image\n$URL")
