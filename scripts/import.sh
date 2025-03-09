407ba7e5f4e5264095741f455ca1ff7cd98e4b93
diced <git@diced.sh>
2025-03-09 15:53:52 -0700

    remove old

--- scripts/import.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "> git clone https://github.com/diced/dotfiles"
-git clone --bare https://github.com/diced/dotfiles $HOME/.cfg
-/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
-
-# todo: install paru and install all packages
--- scripts/osu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-unzip "$1" -d "/media/games/osu/drive_c/osu/Songs/$(basename "${1%.osz}")"
