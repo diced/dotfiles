22723bd9f13b1ce149602090dbe73a91b0eedcf4
diced <git@diced.sh>
2025-08-20 16:59:07 -0700

    fix: move out of nvimtree buffer when using fzf-lua binds

--- modules/nvf/plugins/fzf-lua.nix
+++ modules/nvf/plugins/fzf-lua.nix
@@ -1,5 +1,17 @@
 { ... }:
 
+let
+  # quick helper function that moves into the next buffer
+  #   if we are in the nvimtree buffer.
+  fzfSafe = func: ''
+    function()
+      if vim.bo.filetype == "NvimTree" then
+        vim.cmd("wincmd l")
+      end
+      require("fzf-lua").${func}()
+    end
+  '';
+in
 {
   vim.fzf-lua = {
     enable = true;
@@ -14,15 +26,15 @@
       mode = "n";
       key = "<leader>ff";
       lua = true;
-      action = ''function() require("fzf-lua").files() end'';
-      desc = "FzfLua: Find Files";
+      action = fzfSafe "files";
+      desc = "fzf-lua: Find Files";
     }
     {
       mode = "n";
       key = "<leader>fg";
       lua = true;
-      action = ''function() require("fzf-lua").live_grep() end'';
-      desc = "FzfLua: Live Grep";
+      action = fzfSafe "live_grep";
+      desc = "fzf-lua: Live Grep";
     }
 
     # lsp
@@ -30,21 +42,21 @@
       mode = "n";
       key = "<leader>flr";
       lua = true;
-      action = ''function() require("fzf-lua").lsp_references() end'';
-      desc = "FzfLua: LSP References";
+      action = fzfSafe "lsp_references";
+      desc = "fzf-lua: LSP References";
     }
     {
       mode = "n";
       key = "<leader>fld";
       lua = true;
-      action = ''function() require("fzf-lua").lsp_declarations() end'';
-      desc = "FzfLua: LSP Declarations";
+      action = fzfSafe "lsp_declarations";
+      desc = "fzf-lua: LSP Declarations";
     }
     {
       mode = "n";
       key = "<leader>flf";
       lua = true;
-      action = ''function() require("fzf-lua").lsp_finder() end'';
+      action = fzfSafe "lsp_finder";
       desc = "FzfLua: LSP Finder";
     }
   ];
