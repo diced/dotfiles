51b5ae41c460b0a7548eca8132557741ee405546
diced <git@diced.sh>
2025-12-29 00:41:32 -0800

    fix: depcreated git paths

--- modules/home/programs/git.nix
+++ modules/home/programs/git.nix
@@ -3,8 +3,12 @@
 {
   programs.git = {
     enable = true;
-    userName = "diced";
-    userEmail = "git@diced.sh";
+
+    settings.user = {
+      name = "diced";
+      email = "git@diced.sh";
+    };
+
     signing = {
       key = "436B2B0FA0DCA354";
       signByDefault = true;
