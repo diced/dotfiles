25e8256f3e3ebe782c0ce0cace03c6ae1f589663
diced <git@diced.sh>
2026-03-13 12:28:25 -0700

    fix: caddy tls1.2 bc ios sucks

--- modules/nixos/caddy/default.nix
+++ modules/nixos/caddy/default.nix
@@ -13,13 +13,13 @@
     extraConfig = ''
       (wc-phx) {
         tls /var/lib/acme/phx-diced-sh/cert.pem /var/lib/acme/phx-diced-sh/key.pem {
-          protocols tls1.3
+          protocols tls1.2 tls1.3
         }
       }
 
       (wc-sjc) {
         tls /var/lib/acme/sjc-diced-sh/cert.pem /var/lib/acme/sjc-diced-sh/key.pem {
-          protocols tls1.3
+          protocols tls1.2 tls1.3
         }
       }
     '';
