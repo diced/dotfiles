77234255745369007aea1d38dd1e6f63960adba1
diced <git@diced.sh>
2026-03-21 22:41:50 -0700

    refactor: remove helium browser

--- modules/hosts/macbook-pro/brew.nix
+++ modules/hosts/macbook-pro/brew.nix
@@ -21,7 +21,6 @@
       # browser
       "brave-browser"
       "firefox"
-      "helium-browser"
 
       # social
       "discord"
--- modules/hosts/macbook-pro/dock.nix
+++ modules/hosts/macbook-pro/dock.nix
@@ -14,10 +14,8 @@
       { app = "/System/Applications/System Settings.app"; }
       { app = "/System/Applications/Utilities/Activity Monitor.app"; }
       { app = "/System/Applications/Calendar.app"; }
-      { app = "/Applications/Helium.app"; }
       { app = "/Applications/Brave Browser.app"; }
       { app = "/Applications/Spotify.app"; }
-      # { app = "/Applications/Microsoft Outlook.app"; }
       { app = "/System/Applications/Mail.app"; }
       { app = "/System/Applications/Phone.app"; }
       { app = "/System/Applications/Messages.app"; }
