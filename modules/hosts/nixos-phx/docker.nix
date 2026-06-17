51c0639aa4f50ae5c831af6a1e9af862b54f0f1c
diced <git@diced.sh>
2026-06-16 23:28:16 -0700

    fix: add phx to dokploy

--- modules/hosts/nixos-phx/docker.nix
+++ modules/hosts/nixos-phx/docker.nix
@@ -11,5 +11,6 @@
     (import "${nixosModules}/docker/umami.nix" { dataDir = "/block/umami"; })
     (import "${nixosModules}/docker/mc/1.21.11" { dataDir = "/block/mc-1.21.11"; })
     (import "${nixosModules}/docker/zipline.nix" { dataDir = "/block/zipline4"; })
+    (import "${nixosModules}/docker/dokploy.nix" { dataDir = "/block/dokploy"; })
   ];
 }
