bb8cda2df1c7410ca36f7ade79589a73b5c74e51
diced <git@diced.sh>
2026-06-17 12:44:18 -0700

    feat: try smth

--- modules/nixos/docker/dokploy.nix
+++ modules/nixos/docker/dokploy.nix
@@ -100,7 +100,7 @@
       reverse_proxy 127.0.0.1:3006
     '';
 
-    "http://zipline.diced.sh".extraConfig = ''
+    "zipline.diced.sh".extraConfig = ''
       reverse_proxy 127.0.0.1:8000
     '';
   };
