9bcabf1ca5f3833d71a72752b5836d2c89f7e7e7
diced <git@diced.sh>
2026-07-21 18:11:32 -0700

    fix: clean up

--- modules/home/programs/brew/mpv.nix
+++ modules/home/programs/brew/mpv.nix
@@ -1,28 +1,30 @@
 { pkgs, lib, ... }:
 
 {
-  home.packages = with pkgs; [
-    duti
-  ];
+  home = {
+    packages = with pkgs; [
+      duti
+    ];
 
-  home.file.".config/mpv/mpv.conf".text = ''
-    sub-font-size=50
-    sub-margin-y=70
+    file.".config/mpv/mpv.conf".text = ''
+      sub-font-size=40
+      sub-margin-y=70
 
-    sub-color='#FFFFFFFF'
-    sub-outline-color='#000000'
-    sub-outline-size=6
-  '';
+      sub-color='#FFFFFFFF'
+      sub-outline-color='#000000'
+      sub-outline-size=6
+    '';
 
-  home.file.".config/duti/video.defaults".text = ''
-    io.mpv mp4 all
-    io.mpv mkv all
-    io.mpv mov all
-    io.mpv avi all
-    io.mpv webm all
-  '';
+    file.".config/duti/video.defaults".text = ''
+      io.mpv mp4 all
+      io.mpv mkv all
+      io.mpv mov all
+      io.mpv avi all
+      io.mpv webm all
+    '';
 
-  home.activation.setDefaultVideoApp = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    run ${pkgs.duti}/bin/duti ~/.config/duti/video.defaults
-  '';
+    activation.setDefaultVideoApp = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      run ${pkgs.duti}/bin/duti ~/.config/duti/video.defaults
+    '';
+  };
 }
